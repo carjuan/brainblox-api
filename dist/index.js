3518c2a9af0d47855f2c0ec79cafa68d768de57a
Juan Carvajal <pcjvan@gmail.com>
2024-09-03 15:41:13 -0500

    untrack dist folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function say({ text }) {
-    return `I am saying ${text}`;
-}
-console.log(say({
-    text: 'helloooooo',
-}));
